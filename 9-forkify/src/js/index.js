9c90aca60b8775044110c1f6378c410c462537ab
Ben Critchley <Critchley_11@hotmail.co.uk>
2020-06-01 18:06:22 +0100

    Updated recipeView

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -3,6 +3,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /*Global State of the the app
@@ -78,6 +79,11 @@ const controlRecipe = async () => {
 
     if (id) {
         //Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
 
         //Create new recipe object
         state.recipe = new Recipe(id);
@@ -99,10 +105,27 @@ const controlRecipe = async () => {
         }
 
         //Render recipe
-        console.log(state.recipe);
+        clearLoader();
+        recipeView.renderRecipe(state.recipe);
     }
 };
 
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//Handling Recipe Button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        //Decrease button is clicked
+        if (state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        //Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+});
